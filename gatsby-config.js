84c99a7665ab12cdfd0f0bcac911dabc4367ab79
bigluckypal010203 <bigluckypal010203@gmail.com>
2021-12-26 18:00:30 +0100

    test images

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     description: `Established since 2003, Mentor Education is an Australian nationally recognised vocational education and training (VET) provider.`,
     author: `@marek.reid`,
   },
+  pathPrefix: `/blog`,
   plugins: [
     {
       resolve: `gatsby-source-contentful`,
