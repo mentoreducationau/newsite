3bfda9b12c0c3e1a9f04394b8f3944df709793ff
bigluckypal010203 <bigluckypal010203@gmail.com>
2021-12-27 14:14:11 +0100

    update student life renderer about images

--- src/rich-text-renderers/ArticleRenderer.js
+++ src/rich-text-renderers/ArticleRenderer.js
@@ -7,10 +7,10 @@ import { Paragraph, Subtitle } from "../styles/Typography.css"
 const ArticleRenderer = ({ node }) => {
   const richTextImages = {}
   node.references.map(reference => {
-    return richTextImages[reference.contentful_id] = {
+    return (richTextImages[reference.contentful_id] = {
       image: reference.gatsbyImageData,
       alt: reference.title,
-    }
+    })
   })
   const options = {
     renderMark: {
@@ -25,11 +25,32 @@ const ArticleRenderer = ({ node }) => {
       [BLOCKS.HEADING_4]: (node, children) => <Subtitle>{children}</Subtitle>,
       [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
         // render the EMBEDDED_ASSET as you need
+        const index = Object.keys(richTextImages).indexOf(
+          node.data.target.sys.id
+        )
         const imageData = richTextImages[node.data.target.sys.id]
         const image = getImage(imageData.image)
 
-        return (
-          <div className="align-center">
+        return index % 2 === 0 ? (
+          <div
+            style={{
+              width: "65%",
+              height: "auto",
+              float: "left",
+              marginRight: "2.5rem",
+            }}
+          >
+            <GatsbyImage image={image} alt={imageData.alt} />
+          </div>
+        ) : (
+          <div
+            style={{
+              width: "65%",
+              height: "auto",
+              float: "right",
+              marginLeft: "2.5rem",
+            }}
+          >
             <GatsbyImage image={image} alt={imageData.alt} />
           </div>
         )
