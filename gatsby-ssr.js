4295e5ccecc173c828fde9a4a4e469349da4b8a0
marekreid <marekreid@gmail.com>
2022-01-28 07:31:11 +0100

    changes random from here

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -26,11 +26,11 @@ const App = ({ element }) => {
 
   return (
     // <ThemeProvider theme={width < 1200 ? Theme.mobile : Theme.desktop}>
-    <ThemeProvider theme={theme}>
-      {/* <GlobalStyles />
-      <Typography /> */}
+//    <ThemeProvider theme={theme}>
+      /* <GlobalStyles />
+      <Typography /> */
       <ActiveMenuProvider>{element}</ActiveMenuProvider>
-    </ThemeProvider>
+   // </ThemeProvider>
   )
 }
 
